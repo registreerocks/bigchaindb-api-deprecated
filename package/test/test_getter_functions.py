c9a2976abc58fc0544d86e706891a2125a756bc0
sabinebertram <sabine.bertram@mailbox.org>
2018-07-03 15:02:11 +0200

    include test

--- package/test/test_getter_functions.py
+++ package/test/test_getter_functions.py
@@ -3,7 +3,7 @@ import pytest
 from bigchaindb_driver import BigchainDB
 
 from src.swagger_server.controllers.getter_functions import \
-    _get_marks_by_address
+    _get_marks_by_address, _get_university_files
 
 
 @mock.patch('bigchaindb_driver.BigchainDB.assets')
@@ -12,6 +12,13 @@ def test_get_marks_by_address(mock_transactions, mock_assets):
     mock_assets.get.return_value = get_mark_search_result()
     mock_transactions.get.side_effect = [get_course_asset(), get_mark_asset()]
     assert(_get_marks_by_address('0x04') == [('Econometrics', 'midterm', 85, 0.25)])
+
+@mock.patch('bigchaindb_driver.BigchainDB.assets')
+def test_get_university_files(mock_assets):
+    mock_assets.get.side_effect = [get_university_search_result(), get_degree_search_result()]
+    university_name = "UCT"
+    filetype = 'degree'
+    assert(_get_university_files(university_name, filetype) == get_degree_search_result())
 
 def get_mark_search_result():
     return [{
@@ -35,7 +42,8 @@ def get_course_asset():
                     'weighting': 0.75, 
                     'required': True}],
                 'prerequisite': [],
-                'corequisite': []},
+                'corequisite': [],
+                'university_id': "ece3537ac407a502e0586fbb8ad76771479cfd0689a38013b91ee77d97452023"},
             'asset': {
                 'data': {
                     'course': {
@@ -51,3 +59,30 @@ def get_mark_asset():
             'course': '6f4a3c43ec664373720ce1f8158b2779cfa0aec85954791a8ca766a1e53ef8bb',
             'type': 'midterm'}}},
             'id': '893e409d441b7f93bbad361053d43d9d9d82e570b5ff39c7fc43d83c96e509b0'}]
+
+def get_university_search_result():
+    return [{
+                "data": {
+                "university": {
+                    "name": "University of Cape Town",
+                    "physical_address": "string",
+                    "postal_address": "string",
+                    "short": "UCT"
+                }
+                },
+                "id": "ece3537ac407a502e0586fbb8ad76771479cfd0689a38013b91ee77d97452023"
+            }]
+
+def get_degree_search_result():
+    return [{
+                "data": {
+                "degree": {
+                    "description": "This degree ...",
+                    "id": "string",
+                    "level": "string",
+                    "name": "FinTech2",
+                    "university_id": "ece3537ac407a502e0586fbb8ad76771479cfd0689a38013b91ee77d97452023"
+                }
+                },
+                "id": "527dacbb2dfb75ddab97e8e7b90733366d9d94a57ec9645c8775b58648946a2c"
+            }]
