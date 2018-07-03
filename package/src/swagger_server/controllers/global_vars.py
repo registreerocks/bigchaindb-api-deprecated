281c83e5abebfaaa2c595999f0309c7a154c0213
sabinebertram <sabine.bertram@mailbox.org>
2018-07-03 13:36:43 +0200

    dockerizes API

--- package/src/swagger_server/controllers/global_vars.py
+++ package/src/swagger_server/controllers/global_vars.py
@@ -3,4 +3,4 @@ from bigchaindb_driver import BigchainDB
 from bigchaindb_driver.crypto import generate_keypair
 
 ADMIN = generate_keypair()
-BDB = BigchainDB(os.getenv("BDB_ROOT_URL"))
+BDB = BigchainDB('http://bigchaindb:9984')
