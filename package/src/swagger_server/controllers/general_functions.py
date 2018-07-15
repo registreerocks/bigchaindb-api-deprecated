b1bebd56d3e22dea8f8befff4d169ae2b95b55cf
sabinebertram <sabine.bertram@mailbox.org>
2018-07-15 20:55:07 +0200

    adds timestamp

--- package/src/swagger_server/controllers/general_functions.py
+++ package/src/swagger_server/controllers/general_functions.py
@@ -1,7 +1,14 @@
+import datetime
+
 from .global_vars import BDB
+
 
 def _fulfill_transaction(transaction, key):
     return BDB.transactions.fulfill(transaction, private_keys=key)
 
 def _send_transaction(transaction):
     return BDB.transactions.send_commit(transaction)
+
+def _add_timestamp(dictionary):
+    dictionary['timestamp'] = datetime.datetime.utcnow()
+    return dictionary
