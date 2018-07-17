9fba3944f9d9a8b45783d2b7524209f9700941c1
sabinebertram <sabine.bertram@mailbox.org>
2018-07-17 21:21:03 +0200

    updates mark getter

--- package/test/test_getter_functions.py
+++ package/test/test_getter_functions.py
@@ -22,8 +22,8 @@ def test_get_all_assets(mock_assets, mock_transactions):
 @mock.patch('bigchaindb_driver.BigchainDB.transactions')
 def test_get_marks_by_address(mock_transactions, mock_assets):
     mock_assets.get.return_value = get_mark_assets()
-    mock_transactions.get.side_effect = [get_course_transaction(), get_mark_transaction()]
-    assert(_get_marks_by_address('0x04') == [('Econometrics', 'midterm', 85, 0.25)])
+    mock_transactions.get.return_value = get_mark_transaction()
+    assert(_get_marks_by_address('0x03') == get_mark_search_result())
 
 @mock.patch('bigchaindb_driver.BigchainDB.transactions')
 @mock.patch('bigchaindb_driver.BigchainDB.assets')
@@ -57,18 +57,29 @@ def get_mark_assets():
     return [{
         'data': {
             'asset_type': 'mark',
-            'student': '0x04',
-            'course': '6f4a3c43ec664373720ce1f8158b2779cfa0aec85954791a8ca766a1e53ef8bb',
+            'student_address': '0x03',
+            'course_id': '6f4a3c43ec664373720ce1f8158b2779cfa0aec85954791a8ca766a1e53ef8bb',
             'type': 'midterm'},
         'id': '893e409d441b7f93bbad361053d43d9d9d82e570b5ff39c7fc43d83c96e509b0'}]
 
 def get_mark_transaction():
     return [{
             'metadata': {'mark': 85},
-            'asset': {'data': {'mark': {'student': '0x03',
-            'course': '6f4a3c43ec664373720ce1f8158b2779cfa0aec85954791a8ca766a1e53ef8bb',
+            'asset': {'data': {'mark': {'student_address': '0x03',
+            'course_id': '6f4a3c43ec664373720ce1f8158b2779cfa0aec85954791a8ca766a1e53ef8bb',
             'type': 'midterm'}}},
             'id': '893e409d441b7f93bbad361053d43d9d9d82e570b5ff39c7fc43d83c96e509b0'}]
+
+def get_mark_search_result():
+    return [{
+        'data': {
+            'asset_type': 'mark',
+            'student_address': '0x03',
+            'course_id': '6f4a3c43ec664373720ce1f8158b2779cfa0aec85954791a8ca766a1e53ef8bb',
+            'type': 'midterm'},
+        'id': '893e409d441b7f93bbad361053d43d9d9d82e570b5ff39c7fc43d83c96e509b0',
+        'metadata': {'mark': 85}
+    }]
 
 def get_university_search_result():
     return [{
