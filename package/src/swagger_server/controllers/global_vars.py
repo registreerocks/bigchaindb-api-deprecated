e0192ee737a6ac8657b7add61cb184480e1827ea
Sabine <sabine.bertram@mailbox.org>
2018-11-22 16:23:15 +0100

    new get_mark_by_student using pymongo

--- package/src/swagger_server/controllers/global_vars.py
+++ package/src/swagger_server/controllers/global_vars.py
@@ -1,6 +1,9 @@
 import os
 from bigchaindb_driver import BigchainDB
 from bigchaindb_driver.crypto import generate_keypair
+from pymongo import MongoClient
 
 ADMIN = generate_keypair()
 BDB = BigchainDB('http://bigchaindb:9984')
+MC = MongoClient('mongodb', 27017)
+MDB = MC.bigchain
